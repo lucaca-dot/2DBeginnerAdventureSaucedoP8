44bd54c42929105c441276fadc8a5984c837ea36
lucaca-dot <179270779+lucaca-dot@users.noreply.github.com>
2024-11-14 15:26:45 -0800

    I added boundries projectiles, and camera
    I added to my map. made boundaries, made projectiles, robot animation when fixed. and made the camera follow my character.

--- 2DBeginnerAdventureP8Saucedo/Assets/Scripts/Projectile.cs
+++ 2DBeginnerAdventureP8Saucedo/Assets/Scripts/Projectile.cs
@@ -6,7 +6,7 @@ public class Projectile : MonoBehaviour
 {
     Rigidbody2D rigidbody2d;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
     }
@@ -14,7 +14,10 @@ public class Projectile : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (transform.position.magnitude > 1000.0f)
+        {
+            Destroy(gameObject);
+        }
     }
 
     public void Launch(Vector2 direction, float force)
@@ -24,7 +27,12 @@ public class Projectile : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D other)
     {
-        Debug.Log("projectile Collision with" + other.gameObject);
+        EnemyController e = other.collider.GetComponent<EnemyController>();
+        if (e != null)
+        {
+            e.Fix();
+        }
+        
         Destroy(gameObject);
     }
 }
