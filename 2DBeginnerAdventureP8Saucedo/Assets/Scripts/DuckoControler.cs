5df1fe992decf3fc620ed8683646f1df190e0cb1
lucaca-dot <179270779+lucaca-dot@users.noreply.github.com>
2024-11-18 15:28:49 -0800

    Make smoke effects, and UI health bar
    I made code for smoke, and UI health bar. I completed 2 videos.

--- 2DBeginnerAdventureP8Saucedo/Assets/Scripts/DuckoControler.cs
+++ 2DBeginnerAdventureP8Saucedo/Assets/Scripts/DuckoControler.cs
@@ -86,7 +86,7 @@ public class DuckoControler : MonoBehaviour
             invincibleTimer = timeInvincible;
         }
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
-        Debug.Log(currentHealth + "/" + maxHealth);
+        UIHealthBar.instance.setValue(currentHealth/(float)maxHealth);
     }
 
     void Launch()
--- 2DBeginnerAdventureP8Saucedo/Assets/Scripts/EnemyController.cs
+++ 2DBeginnerAdventureP8Saucedo/Assets/Scripts/EnemyController.cs
@@ -9,6 +9,8 @@ public class EnemyController : MonoBehaviour
     public bool vertical;
     public float changeTime = 3.0f;
 
+    public ParticleSystem smokeEffect;
+
     Rigidbody2D rigidbody2d;
 
     bool broken = true;
@@ -77,5 +79,6 @@ public class EnemyController : MonoBehaviour
         broken = false;
         rigidbody2d.simulated = false;
         animator.SetTrigger("Fixed");
+        smokeEffect.Stop();
     }
 }
